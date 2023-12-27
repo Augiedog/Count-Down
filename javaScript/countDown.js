3b746cfd225efeb6956af4631a8da7ae8f3723fd
Augie Bren <Augie>
2023-12-27 12:53:58 -0600

    new year

--- javaScript/countDown.js
+++ javaScript/countDown.js
@@ -1,22 +1,22 @@
-const days =document.querySelector("#days");
-const hours =document.querySelector("#hours");
-const min =document.querySelector("#min");
-const sec =document.querySelector("#sec");
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
 
-function UpdateTimer(){
+function UpdateTimer() {
     let currentYear = new Date().getFullYear();
-const newYearDate = new Date(`January 1 ${currentYear + 1} 00:00:00`);
-const currentDate = new Date();
-const different = newYearDate - currentDate;
-const d = Math.floor(different / 1000 / 60 / 60 / 24);
-const h = Math.floor((different / 1000 / 60 / 60) % 24);
-const m = Math.floor((different / 1000 / 60) % 60);
-const s = Math.floor((different / 1000) % 60);
+    const newYearDate = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+    const currentDate = new Date();
+    const different = newYearDate - currentDate;
+    const d = Math.floor(different / 1000 / 60 / 60 / 24);
+    const h = Math.floor((different / 1000 / 60 / 60) % 24);
+    const m = Math.floor((different / 1000 / 60) % 60);
+    const s = Math.floor((different / 1000) % 60);
 
-days.innerHTML =d<10?"0"+d:d;
-hours.innerHTML =h<10?"0"+h:h;
-min.innerHTML =m<10?"0"+m:m;
-sec.innerHTML =s<10?"0"+s:s;
+    days.innerHTML = d < 10 ? "0" + d : d;
+    hours.innerHTML = h < 10 ? "0" + h : h;
+    min.innerHTML = m < 10 ? "0" + m : m;
+    sec.innerHTML = s < 10 ? "0" + s : s;
 }
 
-setInterval(UpdateTimer,1000);
+setInterval(UpdateTimer, 1000);
